c07e2b95c7a065f694d976bd70af6e77a39a3dd7
luz <herrera.heidi85@gmail.com>
2026-05-20 09:29:26 -0600

    Segunda practica de clases

--- Clases.py
+++ Clases.py
@@ -6,7 +6,7 @@
     def sample_method(self):
         print(self.name.upper())'''
         
-class Dog:
+'''class Dog:
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -19,4 +19,20 @@ dog_2 = Dog('Thatcher', 5)
         
         
 dog_1.bark()
-dog_2.bark()
+dog_2.bark()'''
+
+class Dog:
+    species = 'French bulldog'
+    
+    def __init__(self, name):
+        self.name = name
+        
+print(Dog.species)
+
+dog1= Dog('Jack')
+print(dog1.name)
+print(dog1.species)
+
+dog2 = Dog('Tom')
+print(dog2.name)
+print(dog2.species)

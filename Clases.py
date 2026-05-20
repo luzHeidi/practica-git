50f8d8948e5141e763884590c1247db9c3b5130a
luz <herrera.heidi85@gmail.com>
2026-05-20 10:10:59 -0600

    practica con clases

--- Clases.py
+++ Clases.py
@@ -37,7 +37,7 @@ dog2 = Dog('Tom')
 print(dog2.name)
 print(dog2.species)'''
 
-class Car:
+'''class Car:
     def __init__(self, color, model):
         self.color = color
         self.model = model
@@ -49,4 +49,27 @@ print(car_1.model)
 print(car_2.model)
 
 print(car_1.color)
-print(car_2.color)
+print(car_2.color)'''
+
+class Book:
+    def __init__(self, title, pages):
+        self.title = title
+        self.pages = pages
+        
+    def __len__(self):
+        return self.pages
+    
+    def __str__(self):
+        return f'´{self.title}´ has {self.pages} pages'
+    
+    def __eq__(self, other):
+        return self.pages == other.pages
+    
+book1 = Book('Built Wealth Like a Boss', 420)
+book2 = Book('Be Your Own Start', 420)
+
+print(len(book1))
+print(len(book2))
+print(str(book1))
+print(str(book2))
+print(book1 == book2)

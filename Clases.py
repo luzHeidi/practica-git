f4f9a2773b7903da194130117d5d844372d4a502
luz <herrera.heidi85@gmail.com>
2026-05-20 09:44:50 -0600

    clase car

--- Clases.py
+++ Clases.py
@@ -21,7 +21,7 @@ dog_2 = Dog('Thatcher', 5)
 dog_1.bark()
 dog_2.bark()'''
 
-class Dog:
+'''class Dog:
     species = 'French bulldog'
     
     def __init__(self, name):
@@ -35,4 +35,18 @@ print(dog1.species)
 
 dog2 = Dog('Tom')
 print(dog2.name)
-print(dog2.species)
+print(dog2.species)'''
+
+class Car:
+    def __init__(self, color, model):
+        self.color = color
+        self.model = model
+        
+car_1 = Car('red', 'Toyota Corolla')
+car_2 = Car('greem', 'Lamborghini Revuelto')
+
+print(car_1.model)
+print(car_2.model)
+
+print(car_1.color)
+print(car_2.color)
